b6eb0c7e71a91ef5131dde3aea77602712b16e24
m-u-ba <85790167+m-u-ba@users.noreply.github.com>
2021-06-19 10:43:05 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,13 +4,13 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b><u>Hey {}!!</b></u>
+<i>Am ✰ 𝙼𝚞𝚋𝚒369 ✰  Auto Filter Bot....😉
 
 Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@Mubi369
 </i>"""    
     
     HELP_TEXT = """
@@ -43,12 +43,12 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@Mubi369
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Mubi369">Mubi_369</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
